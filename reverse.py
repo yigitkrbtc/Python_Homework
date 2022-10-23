d892c10fb4d57ff9f4ce3e58a82a179a401f3e64
yigitkrbtc <112809652+yigitkrbtc@users.noreply.github.com>
2022-10-23 16:04:20 +0300

    Create reverse.py

---
+++ reverse.py
@@ -0,0 +1,22 @@
+def reverse(x):
+    
+    for i in range(int(len(x)/2)):
+        temp = x[i]
+        x[i] = x[len(x)-(i+1)]
+        x[len(x)-(i+1)] = temp
+        
+    counter = 0
+    for i in x:
+        if isinstance(i,list):
+             
+            for a in range(int(len(i)/2)):
+                temp = x[counter][a]
+                x[counter][a] = x[counter][len(i)-(a+1)]
+                x[counter][len(i)-(a+1)] = temp
+                counter +=1
+    print(x)
+                
+
+
+x = [[1, 2], [3, 4], [5, 6, 7]] # given input 
+reverse(x)
